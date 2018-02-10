2422aa40d397d1b7a3282dcfa0cb36626df4a147
CranfieldCoin <36126892+CranfieldCoin@users.noreply.github.com>
2018-02-10 10:15:08 +0000

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -75,7 +75,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "cranfieldcoin";
 
-const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001bb9c01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f21a411d6fef588f93d38c0dbfe0a20bc0a0115f02f077edf056df181cfe2c84";
+const char GENESIS_COINBASE_TX_HEX[] = "";
 
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
